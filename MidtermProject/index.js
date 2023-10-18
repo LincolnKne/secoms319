e5af0fe8d1ba56eea4659b5708fcba1b69895ce7
Lincolnkne <Lincoln.Kness@gmail.com>
2023-10-18 13:51:15 -0500

    added

--- MidtermProject/index.js
+++ MidtermProject/index.js
@@ -1,10 +1,14 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+function setProgressBar(taskElement, percentage) {
+  const progressBarFilled = taskElement.querySelector('.progress-bar-filled');
+  progressBarFilled.style.width = `${percentage}%`;
+}
